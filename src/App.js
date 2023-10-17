c997ecd8314e6de25c39cb9eae74438872a197e6
Sujoy Dhar <sujoydhar95@gmail.com>
2023-10-17 11:27:17 +0600

    builspec.yml file added

--- src/App.js
+++ src/App.js
@@ -3,22 +3,14 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className="App">
+     <h1>Hello,This ia a React website hosted in S3</h1>
+     <h3>Have a Nice day</h3>
     </div>
+
+    </div>
+    
   );
 }
 
